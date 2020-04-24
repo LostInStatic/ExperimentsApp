87a16b07af7787bacf014450c9a0d5984f2545db
Cezary Czwal <czwalcezary@me.com>
2020-04-24 21:20:27 +0200

    Init commit

---
+++ src/main/java/pl/czarczeslaw/experimentsapp/ExperimentsappApplication.java
@@ -0,0 +1,13 @@
+package pl.czarczeslaw.experimentsapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExperimentsappApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ExperimentsappApplication.class, args);
+    }
+
+}
---
+++ src/test/java/pl/czarczeslaw/experimentsapp/ExperimentsappApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.czarczeslaw.experimentsapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExperimentsappApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
